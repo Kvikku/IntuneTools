4d021f521fb9c6f87bbc4c43b3be459a6b875c89
Jørgen <85877485+Kvikku@users.noreply.github.com>
2025-02-04 08:12:11 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using IntuneTools.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace IntuneTools
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            //myButton.Content = "Clicked";
+        }
+
+        
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavigateToPage(navItemTag);
+            }
+        }
+
+        private void NavigateToPage(string navItemTag)
+        {
+            switch (navItemTag)
+            {
+                case "home":
+                    ContentFrame.Navigate(typeof(BlankPage1));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
