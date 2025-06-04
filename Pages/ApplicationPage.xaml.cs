62e51f48bae19f3dd9c7ee49341e5dda29a6822d
Jørgen Sundet <85877485+Kvikku@users.noreply.github.com>
2025-06-04 14:48:02 +0200

    Add DataGrid to ApplicationPage for app listing
    Updated `ApplicationPage.xaml` to include a `DataGrid`
    for displaying a list of applications with columns for
    "App Name", "App ID", and "Version". Introduced a new
    class `AppInfo` in `ApplicationPage.xaml.cs` to represent
    application data, and populated an `ObservableCollection<AppInfo>`
    with sample data for the `DataGrid`.

--- Pages/ApplicationPage.xaml.cs
+++ Pages/ApplicationPage.xaml.cs
@@ -12,20 +12,35 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.Collections.ObjectModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace IntuneTools.Pages
 {
+    public class AppInfo
+    {
+        public string AppName { get; set; }
+        public string AppId { get; set; }
+        public string Version { get; set; }
+    }
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class ApplicationPage : Page
     {
+        public ObservableCollection<AppInfo> AppList { get; set; } = new ObservableCollection<AppInfo>();
+
         public ApplicationPage()
         {
             this.InitializeComponent();
+            // Sample data
+            AppList.Add(new AppInfo { AppName = "App One", AppId = "001", Version = "1.0" });
+            AppList.Add(new AppInfo { AppName = "App Two", AppId = "002", Version = "2.0" });
+            AppList.Add(new AppInfo { AppName = "App Three", AppId = "003", Version = "3.0" });
+            AppDataGrid.ItemsSource = AppList;
         }
     }
 }
